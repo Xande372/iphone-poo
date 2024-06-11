6ea64356f0105a7f53ac28406a93afca35ab3730
Alexandre Direito <xandedsj@gmail.com>
2024-06-11 12:09:25 -0300

    feito

--- iphone/src/aparelho/Iphone.java
+++ iphone/src/aparelho/Iphone.java
@@ -1,7 +1,12 @@
 package aparelho;
 
-public class Iphone {
+import aparelho.aparelhoTelefonico.AparelhoTelefonico;
+import aparelho.navegadorInternet.Navegador;
+import aparelho.reprodutorMusical.Reprodutor;
+
+public abstract class Iphone implements Reprodutor, Navegador, AparelhoTelefonico {
     public static void main(String[] args) throws Exception {
+    
         
-    }
+    };
 }
